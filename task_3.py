53f0a5206dda9f6574585dfc6f4fa968cd821eeb
AustinHongDru <austin970609@gmail.com>
2022-05-10 00:37:37 +0900

    third

--- task_3.py
+++ task_3.py
@@ -4,14 +4,10 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-urls = ['https://www.youtube.com/watch?v=HbbyfQbJen4', 'https://www.youtube.com/watch?v=MclZ349RVf8', 'https://www.youtube.com/watch?v=Kbn-dNTpSM4', 'https://www.youtube.com/watch?v=sy_12FM0Xp8', 'https://www.youtube.com/watch?v=389itBs7YlE']
-videoIDs = []
-for i in urls:
-    videoIDs.append (i[32:])
-print (videoIDs)
+
 comments = list()
 api_obj = build('youtube', 'v3', developerKey='AIzaSyCgmJab8xSvJyaEXhIxDR-vtNU-w1sUEr4')
-for i in videoIDs:
+for i in video_id_list:
     response = api_obj.commentThreads().list(part='snippet,replies', videoId=i, maxResults=100).execute()
 
     while response:
